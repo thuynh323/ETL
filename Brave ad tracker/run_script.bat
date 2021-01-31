51c56754a01654b2a3ee2e80678b252345e2d172
thuynh323 <thanhhuynhall@outlook.com>
2021-01-30 20:06:38 -0500

    added choices in .bat file and reran

--- Brave ad tracker/run_script.bat
+++ Brave ad tracker/run_script.bat
@@ -1,3 +1,18 @@
+@echo off
+:Ask
+echo Sorry...Have I run today? Please check the database if you don't remember. (y/n)
+set INPUT=
+set /P INPUT=Type input: %=%
+If /I "%INPUT%"=="y" goto yes 
+If /I "%INPUT%"=="n" goto no
+echo Incorrect input & goto Ask
+
+:yes
+echo You should get some sleep...
+pause
+
+:no
+echo Start scraping...
 cd C:\Users\Thanh Huynh\Documents\Projects\brave-ads\venv
 "C:\Python38\python.exe" "C:\Users\Thanh Huynh\Documents\Projects\brave-ads\venv\main.py"
 pause
