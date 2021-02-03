48a585a688c1665d9bc72cc736491d831bc79cd4
thuynh323 <thanhhuynhall@outlook.com>
2021-02-02 22:04:26 -0500

    split into functions

--- Email auto responder/responder.py
+++ Email auto responder/responder.py
@@ -5,11 +5,9 @@ from urllib3.exceptions import InsecureRequestWarning
 import win32com.client as win32
 from usps import USPSApi
 import re
-import json
 
-USER_ID = '' # Your USPS API username
-EMAIL_ACCOUNT = '' # Your Outlook email address
-FIRST_FOLDER = '' # Your folder/ first folder
+USER_ID = # Your USPS API username
+EMAIL_ACCOUNT = # Your Outlook account
 
 old_merge_environment_settings = requests.Session.merge_environment_settings
 
@@ -25,21 +23,17 @@ def no_ssl_verification():
         # all the opened adapters once we're done. Otherwise, the effects of
         # verify=False persist beyond the end of this context manager.
         opened_adapters.add(self.get_adapter(url))
-
         settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
         settings['verify'] = False
-
         return settings
 
     requests.Session.merge_environment_settings = merge_environment_settings
-
     try:
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', InsecureRequestWarning)
             yield
     finally:
         requests.Session.merge_environment_settings = old_merge_environment_settings
-
         for adapter in opened_adapters:
             try:
                 adapter.close()
@@ -74,23 +68,19 @@ def tracking_summary(tracking_number: str, tracking_status: dict) -> str:
         if pic_city != None:
             mail_body = f"""
                 <html>
-                <style>
-                    .tab {
-                        tab-size: 8;
-                    }
-                </style>
+                <title></title>
                 <body>
                     <font face='Calibri' size='-0.5'>
                     <p>Hello,</p>
                     <p>Your request has been received and is being reviewed by our support department. 
                     While we investigate this package, we have set up an email alert with USPS for you
                     to receive updates until the package is delivered.</p>
-                    Tracking number:<br>
-                    <pre class='tab'>   {tracking_number}<br></pre>
+                    <p>Tracking number:<br>
+                    &emsp;{tracking_number}<br>
                     Current package status:<br>
-                    <pre class='tab'>   {pic_status}&emsp;{event_date} {event_time}<br></pre>
+                    &emsp;{pic_status}&emsp;{event_date} {event_time}<br>
                     USPS Entry Point:<br>
-                    <pre class='tab'>   {pic_city}, {pic_state} {pic_zip}</pre></p>
+                    &emsp;{pic_city}, {pic_state} {pic_zip}</p>
                     </font>
                 </body>
                 </html>
@@ -98,70 +88,62 @@ def tracking_summary(tracking_number: str, tracking_status: dict) -> str:
         else:
             mail_body = f"""
                 <html>
-                <style>
-                    .tab {
-                        tab-size: 8;
-                    }
-                </style>
                 <body>
                     <font face='Calibri' size='-0.5'>
                     <p>Hello,</p>
                     <p>Your request has been received and is being reviewed by our support department. 
                     While we investigate this package, we have set up an email alert with USPS for you
                     to receive updates until the package is delivered.</p>
-                    Tracking number:<br>
-                    <pre class='tab'>   {tracking_number}<br></pre>
+                    <p>Tracking number:<br>
+                    &emsp;{tracking_number}<br>
                     Current package status:<br>
-                    <pre class='tab'>   {pic_status}</pre></p>
+                    &emsp;{pic_status}</p>
                     </font>
                 </body>
                 </html>
                 """
-    if pic_error == '-2147219284':
+    elif pic_error == '-2147219284':
         mail_body = f"""
                 <html>
-                <style>
-                    .tab {
-                        tab-size: 8;
-                    }
-                </style>
                 <body>
                     <font face='Calibri' size='-0.5'>
                     <p>Hello,</p>
-                    <p>Your request has been received and is being reviewed by our support department. 
-                    While we investigate this package, we have set up an email alert with USPS for you
-                    to receive updates until the package is delivered.</p>
-                    Tracking number:<br>
-                    <pre class='tab'>   {tracking_number}<br></pre>
-                    Current package status:<br>
-                    <pre class='tab'>   {pic_status}</pre></p>
+                    <p>Your request has been received and is being reviewed by our support department.
+                    This package has currently been received by its destination processing facility.</p>
+                    <p>Tracking number:<br>
+                    &emsp;{tracking_number}<br></p>
                     </font>
                 </body>
                 </html>
                 """
+    else:
+        mail_body = tracking_status.get('Description')
     return mail_body
 
-outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
-account = outlook.Folders[EMAIL_ACCOUNT]
-folder = account.Folders[FIRST_FOLDER]
-read_folder = folder.Folders['Test']
-
-with no_ssl_verification():
-
-    for mail in read_folder.Items:
+def reply_mail(mail_items):
+    for mail in mail_items:
         if '_MailItem' in str(type(mail)):
             mail_content = mail.Body
-            find_tracking_number = re.findall(r'92\d{24}', mail_content)
-            if len(find_tracking_number) == 1:
-                tracking_number = find_tracking_number[0]
+            find_tracking_number = re.search(r'92\d{24}', mail_content)
+            if find_tracking_number != None:
+                tracking_number = find_tracking_number.group(0)
                 tracking_result = track_pic(tracking_number)
                 reply_all = mail.ReplyAll()
-                if 'Number' in tracking_result:
-                    reply_all.HTMLBody = f"Hello,\n\n{tracking_result.get('Description')}.\n\nHave a great day!" + reply_all.HTMLBody
-                else:
-                    reply_all.HTMLBody = tracking_summary(tracking_number, tracking_result) + reply_all.HTMLBody
+                reply_all.HTMLBody = tracking_summary(tracking_number, tracking_result) + reply_all.HTMLBody
                 reply_all.Save()
             else:
                 continue
         else:
             continue
+
+def main():
+    outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
+    account = outlook.Folders[EMAIL_ACCOUNT]
+    folder = account.Folders['Inbox']
+    read_folder = folder.Folders['Test'] # Change with the appropriate folder
+
+    with no_ssl_verification():
+        reply_mail(read_folder.Items)
+
+if __name__ == '__main__':
+    main()
