fbab2b21f8353f648b957589f1af1cd99dec147c
thuynh323 <thanhhuynhall@outlook.com>
2021-02-26 12:43:49 -0500

    added config parser to read config file
    adjusted function parameters to generalize

--- Email auto responder/responder.py
+++ Email auto responder/responder.py
@@ -1,181 +1,36 @@
-import warnings
-import contextlib
 import requests
-from urllib3.exceptions import InsecureRequestWarning
 import win32com.client as win32
-from usps import USPSApi
+import configparser
+import json
 import re
-
-USER_ID = # Your USPS API username
-EMAIL_ACCOUNT = # Your Outlook account
-
-old_merge_environment_settings = requests.Session.merge_environment_settings
-
-@contextlib.contextmanager
-def no_ssl_verification():
-    """
-    Source: https://stackoverflow.com/a/15445989
-    """
-    opened_adapters = set()
-
-    def merge_environment_settings(self, url, proxies, stream, verify, cert):
-        # Verification happens only once per connection so we need to close
-        # all the opened adapters once we're done. Otherwise, the effects of
-        # verify=False persist beyond the end of this context manager.
-        opened_adapters.add(self.get_adapter(url))
-        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
-        settings['verify'] = False
-        return settings
-
-    requests.Session.merge_environment_settings = merge_environment_settings
-    try:
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', InsecureRequestWarning)
-            yield
-    finally:
-        requests.Session.merge_environment_settings = old_merge_environment_settings
-        for adapter in opened_adapters:
-            try:
-                adapter.close()
-            except:
-                pass
-
-def track_pic(tracking_number: str) -> dict:
-    """
-    Returns a dictionary of tracking summary or tracking error
-    """
-    usps = USPSApi(USER_ID)
-    response = usps.track(tracking_number).result.get('TrackResponse').get('TrackInfo')
-
-    if 'TrackSummary' in response:
-        result = response.get('TrackSummary')
-    elif 'Error' in response:
-        result = response.get('Error')
-    else:
-        pass
-    return result
-
-def tracking_summary(tracking_number: str, tracking_status: dict) -> str:
-    
-    pic_error = tracking_status.get('Number')
-    pic_status = tracking_status.get('Event')
-    pic_city = tracking_status.get('EventCity')
-    pic_state = tracking_status.get('EventState')
-    pic_zip = tracking_status.get('EventZIPCode')
-    event_date = tracking_status.get('EventDate')
-    event_time = tracking_status.get('EventTime')
-    if pic_error == None:
-        if pic_city != None:
-            mail_body = f"""
-                <html>
-                <title></title>
-                <body>
-                    <font face='Calibri' size='-0.5'>
-                    <p>Hello,</p>
-                    <p>Your request has been received and is being reviewed by our support department. 
-                    While we investigate this package, we have set up an email alert with USPS for you
-                    to receive updates until the package is delivered.</p>
-                    <p>Tracking number:<br>
-                    &emsp;{tracking_number}<br>
-                    Current package status:<br>
-                    &emsp;{pic_status}&emsp;{event_date} {event_time}<br>
-                    USPS Entry Point:<br>
-                    &emsp;{pic_city}, {pic_state} {pic_zip}</p>
-                    </font>
-                </body>
-                </html>
-                """
-        else:
-            mail_body = f"""
-                <html>
-                <body>
-                    <font face='Calibri' size='-0.5'>
-                    <p>Hello,</p>
-                    <p>Your request has been received and is being reviewed by our support department. 
-                    While we investigate this package, we have set up an email alert with USPS for you
-                    to receive updates until the package is delivered.</p>
-                    <p>Tracking number:<br>
-                    &emsp;{tracking_number}<br>
-                    Current package status:<br>
-                    &emsp;{pic_status}</p>
-                    </font>
-                </body>
-                </html>
-                """
-    elif pic_error == '-2147219284':
-        mail_body = f"""
-                <html>
-                <body>
-                    <font face='Calibri' size='-0.5'>
-                    <p>Hello,</p>
-                    <p>Your request has been received and is being reviewed by our support department.
-                    This package has currently been received by its destination processing facility.</p>
-                    <p>Tracking number:<br>
-                    &emsp;{tracking_number}<br></p>
-                    </font>
-                </body>
-                </html>
-                """
-    else:
-        mail_body = tracking_status.get('Description')
-    return mail_body
-
-def reply_mail(mail_items):
-    for mail in mail_items:
-        if '_MailItem' in str(type(mail)):
-            mail_content = mail.Body
-            find_tracking_number = re.search(r'92\d{24}', mail_content)
-            if find_tracking_number != None:
-                tracking_number = find_tracking_number.group(0)
-                tracking_result = track_pic(tracking_number)
-                reply_all = mail.ReplyAll()
-                reply_all.HTMLBody = tracking_summary(tracking_number, tracking_result) + reply_all.HTMLBody
-                reply_all.Save()
-            else:
-                continue
-        else:
-            continue
-
-def main():
-    outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
-    account = outlook.Folders[EMAIL_ACCOUNT]
-    folder = account.Folders['Inbox']
-    read_folder = folder.Folders['Test'] # Change with the appropriate folder
-
-    with no_ssl_verification():
-        reply_mail(read_folder.Items)
-
-if __name__ == '__main__':
-    main()
-    
- 
-##########################--------------------------------------------------------################################
-
-import requests
-import win32com.client as win32
 from datetime import datetime
-import re
+from os import environ
+from pathlib import Path
 
-EMAIL_ACCOUNT = '' # Your Outlook email address
-API_KEY = '' # Your Access License Number
-HEADERS = {"AccessLicenseNumber": API_KEY}
-API_VERSION = 'v1'
-API_BASE_URL = f"https://onlinetools.ups.com/track/{API_VERSION}/details/"
-
-def track_pic(tracking_number: str) -> dict:
+def track_pic(url: str,
+              headers: str,
+              ca_path: str,
+              tracking_number: str) -> dict:
     """
     Returns a dictionary of tracking summary or tracking error
     """
-    r = requests.get(f'{API_BASE_URL}{tracking_number}', headers=HEADERS, verify=False)
-    response = r.json().get('trackResponse').get('shipment')
+    r = requests.get(f'{url}{tracking_number}',
+                     headers=headers,
+                     verify=ca_path)
+    response = r.json().get('trackResponse').get('shipment')[0].get('package')
 
     if response == None:
         tracking_summary = {}
     else:
-        tracking_summary = response[0].get('package')[0].get('activity')[0]
+        tracking_summary = response[0].get('activity')[0]
     return tracking_summary
 
-def tracking_details(tracking_number: str, tracking_summary: dict) -> str:
+def tracking_details(tracking_number: str,
+                     tracking_summary: dict) -> str:
+    """
+    Returns mail content
+    """
+    
     if tracking_summary == {}:
         mail_body = f"""
             <html>
@@ -189,15 +44,15 @@ def tracking_details(tracking_number: str, tracking_summary: dict) -> str:
             </html>
             """
     else:
-        location = tracking_summary.get('address')
+        location = tracking_summary.get('location').get('address')
         status = tracking_summary.get('status')
 
         last_city = location.get('city')
         last_state = location.get('stateProvince')
         last_zip = location.get('postalCode')
         last_status = status.get('description')
-        last_date = datetime.strptime(status.get('date'), '%Y%m%d').date()
-        last_time = datetime.strptime(status.get('date'), '%HH%MM%SS%').time()
+        last_date = datetime.strptime(tracking_summary.get('date'), '%Y%m%d').date()
+        last_time = datetime.strptime(tracking_summary.get('time'), '%H%M%S').time()
     
         if last_city != None:
             mail_body = f"""
@@ -238,16 +93,32 @@ def tracking_details(tracking_number: str, tracking_summary: dict) -> str:
                 """
     return mail_body
 
-def reply_mail(mail_items):
+def reply_mail(mail_items: object,
+               url: str,
+               headers: str,
+               ca_path: str):
+    """
+    Read mail items and extract tracking numbers from the content
+    Draft response emails including the latest tracking event retrieved from API
+    """
+    # Iterarte over mail items
     for mail in mail_items:
+        # Select emails only
         if '_MailItem' in str(type(mail)):
             mail_content = mail.Body
+            # Extract tracking number
             find_tracking_number = re.search(r'92\d{24}', mail_content)
-            reply_all = mail.ReplyAll()
             if find_tracking_number != None:
                 tracking_number = find_tracking_number.group(0)
-                tracking_result = track_pic(tracking_number)
-                mail_body = tracking_details(tracking_number, tracking_result)
+                tracking_result = track_pic(url=url,
+                                            headers=headers,
+                                            ca_path=ca_path,
+                                            tracking_number=tracking_number)
+
+                mail_body = tracking_details(tracking_number=tracking_number,
+                                             tracking_summary=tracking_result)
+                # Draft response in HTML format
+                reply_all = mail.ReplyAll()
                 reply_all.HTMLBody = mail_body + reply_all.HTMLBody
                 reply_all.Save()
             else:
@@ -256,11 +127,39 @@ def reply_mail(mail_items):
             continue
 
 def main():
-    outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
-    account = outlook.Folders[EMAIL_ACCOUNT]
-    folder = account.Folders['Inbox']
-    read_folder = folder.Folders['Test'] # Change with the appropriate folder
-    reply_mail(read_folder.Items)
+    
+    # Read in the config file
+    config = configparser.ConfigParser(interpolation=configparser.BasicInterpolation())
+    config.read('config.ini')
+    
+    email_account = config['DEFAULT']['email_to_read']
+    api_key = config['DEFAULT']['api_key']
+    headers = {'AccessLicenseNumber': api_key}
+    ca_path = config['DEFAULT']['ca_path']
+    url = config['DEFAULT']['base_url']
+    main_folder = config['DEFAULT']['main_folder']
+    sub_folder = config['DEFAULT']['sub_folder']
+    
+    # Access Outlook
+    try:
+        outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
+    except AttributeError:
+        username = environ.get('USERNAME')
+        file_loc = f"C:\\Users\{username}\\AppLocal\\Temp\\gen_py"
+        for f in file_loc:
+            Path.unlink(f)
+        Path.mkdir(f_loc)
+        outlook = win32.gencache.EnsureDispatch('Outlook.Application').GetNamespace('MAPI')
+    # Select email and folder to read    
+    account = outlook.Folders[email_account]
+    folder = account.Folders[main_folder]
+    read_folder = folder.Folders[sub_folder]
+    
+    # Draft responses
+    reply_mail(mail_items=read_folder.Items,
+               url=url,
+               headers=headers,
+               ca_path=ca_path)
 
 if __name__ == '__main__':
     main()
