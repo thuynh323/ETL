6e11e73e775ed303710dacf1d707a145b22f1ee9
thuynh323 <thanhhuynhall@outlook.com>
2021-03-20 12:17:17 -0400

    added set_alert function

--- Email responder with API/draft_responses.py
+++ Email responder with API/draft_responses.py
@@ -1,8 +1,10 @@
 import requests
 import win32com.client as win32
 import configparser
+import socket
 import json
 import re
+from urllib import parse
 from os import environ
 from shutil import rmtree
 from datetime import datetime
@@ -40,17 +42,60 @@ def track_pic(url: str,
         if last_date != None:
             tracking_events['last_date_time'] = str(last_date) + ' ' + str(last_time)
     return tracking_events
+
+def get_url(base_url: str, xml: str) -> str:
+
+    url = '{}&{}'.format(base_url, parse.urlencode({'XML': xml}))
+    return url
+
+def set_alert(field_url: str,
+              email_url: str,
+              ca_path: str,
+              request_field_xml: str,
+              request_email_xml: str,
+              tracking_number: str,
+              email: str):
+
+    """
+    Set email alert for sender to receive future tracking activity
+    """
+    # Get current IP address to access Package Tracking "Fields" API
+    IP_address = socket.gethostbyname(socket.gethostname())
+
+    # Access to Package Tracking "Fields" API to collect MpSuffix and MpDate
+    # These are required to access Tracking and Confirm by Email API
+    request_field_url = get_url(base_url=field_url, 
+                                xml=request_field_xml
+                                .format(IP_address, tracking_number))
+    
+    get_mp_field = requests.get(request_field_url, verify=ca_path)
+    result = get_mp_field.content.decode('utf-8')
+    
+    mp_suffix = re.search(r'[0-9]+(?=</MPSUFFIX>)', result)[0]
+    mp_date = re.search(r'(?<=<MPDATE>)\S+\s\S+(?=</MPDATE>)', result)[0]
+    
+    # Access to Tracking and Confirm by Email API and set email alert
+    request_email_url = get_url(base_url=email_url,
+                                xml=request_email_xml
+                                .format(tracking_number, mp_suffix, mp_date, email))
+    response = requests.get(request_email_url, verify=ca_path)
+    
     
 def main():
     
     config = configparser.ConfigParser(interpolation=configparser.BasicInterpolation())
     config.read('config.txt')
 
-    email_account = config['EMAIL']['email_to_read']
-    ups_key = config['DEFAULT']['ups_key']
-    headers = {'AccessLicenseNumber': ups_key}
     ca_path = config['DEFAULT']['ca_path']
-    url = config['DEFAULT']['base_url']
+    ups_headers = {'AccessLicenseNumber': config['UPS']['ups_key']}
+    ups_url = config['UPS']['ups_url']
+    
+    field_url = config['USPS']['field_url']
+    field_xml = config['USPS']['request_field']
+    email_url = config['USPS']['email_url']
+    email_xml = config['USPS']['request_email']
+
+    email_account = config['EMAIL']['email_to_read']
     main_folder = config['EMAIL']['main_folder']
     sub_folder = config['EMAIL']['sub_folder']
 
@@ -76,8 +121,8 @@ def main():
             
             if find_tracking_number != None and len(find_tracking_number) == 1:
                 tracking_number = find_tracking_number[0]
-                tracking_result = track_pic(url=url,
-                                            headers=headers,
+                tracking_result = track_pic(url=ups_url,
+                                            headers=ups_headers,
                                             ca_path=ca_path,
                                             tracking_number=tracking_number)
                 
@@ -95,6 +140,16 @@ def main():
                         sender = mail.Sender.GetExchangeUser().PrimarySmtpAddress
                     else:
                         sender = mail.SenderEmailAddress
+                        if '@ups.com' in sender.lower():
+                            continue
+                        else:
+                            set_alert(field_url=field_url,
+                                      email_url=email_url,
+                                      ca_path=ca_path,
+                                      request_field_xml=field_xml,
+                                      request_email_xml=email_xml,
+                                      tracking_number=tracking_number,
+                                      email=sender)
                     
                 elif len(tracking_result) == 3:
                     reply_all.HTMLBody = (config['EMAIL']['unregistered_tracking']
